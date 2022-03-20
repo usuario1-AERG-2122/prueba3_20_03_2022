b1d778d9d047ec309dec12439ac0a0444b6a660c
Asensio Rebollo Gomez <rebollo12481@iesmarenostrum.com>
2022-03-20 13:43:47 +0100

    Pongo boton resta

---
+++ Prueba3_20_03_2022/Prueba3_20_03_2022/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Prueba3_20_03_2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tNumero1 = new System.Windows.Forms.TextBox();
+            this.tNumero2 = new System.Windows.Forms.TextBox();
+            this.tResultado = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "suma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado";
+            // 
+            // tNumero1
+            // 
+            this.tNumero1.Location = new System.Drawing.Point(137, 47);
+            this.tNumero1.Name = "tNumero1";
+            this.tNumero1.Size = new System.Drawing.Size(100, 20);
+            this.tNumero1.TabIndex = 4;
+            // 
+            // tNumero2
+            // 
+            this.tNumero2.Location = new System.Drawing.Point(137, 89);
+            this.tNumero2.Name = "tNumero2";
+            this.tNumero2.Size = new System.Drawing.Size(100, 20);
+            this.tNumero2.TabIndex = 5;
+            // 
+            // tResultado
+            // 
+            this.tResultado.Location = new System.Drawing.Point(137, 124);
+            this.tResultado.Name = "tResultado";
+            this.tResultado.Size = new System.Drawing.Size(100, 20);
+            this.tResultado.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Resta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 276);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tResultado);
+            this.Controls.Add(this.tNumero2);
+            this.Controls.Add(this.tNumero1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tNumero1;
+        private System.Windows.Forms.TextBox tNumero2;
+        private System.Windows.Forms.TextBox tResultado;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
